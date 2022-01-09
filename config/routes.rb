8d2a2b827594854a511e400870ef3d2fd02eba82
Nick Francisci <nickfrancisci@gmail.com>
2022-01-09 00:47:59 -0500

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # Defines the root url ("/") as the sign in page
   devise_scope :user do
-    root to: "devise/sessions#new"
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
   end
-
-  # Redirests signing out users back to the root
-  get "users", to: redirect("/")
 
   devise_for :users
 end
